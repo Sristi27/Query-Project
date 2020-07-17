8745256fe2614bf635fdfd69080a8d2715351550
Sristi27 <sristi2705@gmail.com>
2020-07-17 20:29:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function show_value(x) {
+ document.getElementsByClassName("slider_value")[x-1].innerHTML=document.getElementsByClassName("range")[x-1].value;
+}
+  var inputs=document.getElementsByClassName("range");
+  var spans=document.getElementsByClassName("slider_value");
+  for(var i=0;i<inputs.length;i++){
+    spans[i].innerHTML=inputs[i].value;
+  }
+    openNav();
+    closeNav();
