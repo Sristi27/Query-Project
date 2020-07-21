f347c61154ccb51a03b0ed70d01531b08cd58977
Sristi27 <sristi2705@gmail.com>
2020-07-21 18:48:33 +0530

    form added

--- app.js
+++ app.js
@@ -5,14 +5,22 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+function formSubmit(event){
+  console.log( document.getElementById("form").elements.length);
+  event.preventDefault();
+}
 
 function show_value(x) {
  document.getElementsByClassName("slider_value")[x-1].innerHTML=document.getElementsByClassName("range")[x-1].value;
 }
+
   var inputs=document.getElementsByClassName("range");
   var spans=document.getElementsByClassName("slider_value");
   for(var i=0;i<inputs.length;i++){
     spans[i].innerHTML=inputs[i].value;
   }
+
+
     openNav();
     closeNav();
+ 
